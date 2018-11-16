37c6fdcd5c09ae8e11548a3a8554c1848b923c78
Jane Guleski <jguleski@e1z2r3p47.42.us.org>
2018-11-15 16:59:05 -0800

    floating points obidi

--- number_handler.c
+++ number_handler.c
@@ -6,7 +6,7 @@
 /*   By: jguleski <jguleski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 23:34:25 by jguleski          #+#    #+#             */
-/*   Updated: 2018/11/14 16:18:28 by jguleski         ###   ########.fr       */
+/*   Updated: 2018/11/15 16:58:37 by jguleski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ void	transform_unsigned(t_elem *elem)
 		elem->flags[0] = '#';
 }
 
+
 // static char	*pointer(t_plist *list)
 // {
 // 	list->flag[0] = '#';
@@ -72,83 +73,45 @@ void	transform_unsigned(t_elem *elem)
 // 	return (ft_max_itoabase(16, (unsigned long)list->arg, 0));
 // }
 
-void	add_padding(t_elem *elem, int sign, int hash)
+void	handle_floats(t_elem *elem)
 {
-	char	pad;
-	int		justify;
-	int		diff;
-	char	*padding;
-	char	*temp;
+	long long	whole_num;
+	long double	*number;
+	char		*result;
+	int			i;
+	char		*temp;
 
-	pad = ' ';
-	justify = (ft_strchr(elem->flags, '-') == NULL ? 0 : 1);
-	if ((diff = (int)elem->width - (int)ft_strlen((char*)elem->data)) < 1)
-		return ;
-	if (ft_strchr(elem->flags, '0') && justify == 0 && elem->precision == -1) // za precis ovde ne sum sig
-		pad = '0';
-	if (pad == ' ')
-		sign = 0;
-	padding = ft_strnew(diff + sign);
-	while (--diff >= 0)
-		padding[diff + sign] = pad;
-	padding[0] = (sign == 1 ? ((char*)elem->data)[0] : pad);
-	if (justify)
-		temp = ft_strjoin(elem->data, padding);
-	else
-		temp = insert_string(padding, elem->data + sign, hash, pad);//ft_strjoin(padding, elem->data + sign);
-	free(padding);
-	ft_memdel(&(elem->data));
-	elem->data = temp;
+
+	i = 8; // 8 za 7 iteracii
+	//if (elem->length[0] == '\0')
+		number = elem->data;
+	whole_num = (int)(*number);
+	*number = *number - whole_num;
+	result = ft_itoa(whole_num);
+	while (--i)
+		*number = *number * 10;
+	temp = ft_itoa(*number);
+	i = 8;
+	//if (ft_strlen(temp) != i - 1)
+	elem->data = ft_strnew(ft_strlen(result) + i - 1);
+	ft_strcat(elem->data, result);
+	ft_strcat(elem->data, ".");
+	free(result);
+	if ((i = (i - 1) - ft_strlen(temp)) != 0)
+		while (i--)
+			ft_strcat(elem->data, "0");
+	ft_strcat(elem->data, temp);
+	free(temp);
 }
-
-void	handle_prec(t_elem *elem, int digits, int sign)
-{
-	int		diff;
-	char	*temp;
-	// int		sign;
-
-	// sign = 0;
-	// if (ft_isdigit(((char*)(elem->data))[0]) == 0)
-	// 	sign++;
-	diff = elem->precision - (digits - sign);
-	temp = ft_strnew(elem->precision + sign);  // + sign za ako imat + ili ' '
-	ft_strcpy(&temp[diff], (const char*)elem->data); // + sign za da ne go kopirame minusot ako imat
-	while (--diff + sign >= 0)
-		temp[diff + sign] = '0';
-	if (sign)
-		temp[0] = '-';
-	ft_memdel(&(elem->data));
-	elem->data = (void*)temp;
-}
-
-void	num_flags_handler(t_elem *elem)
-{
-	int		length;
-	char	*temp;
-	int		sign;
-
-	temp = NULL;
-	sign = (((char*)(elem->data))[0] == '-' ? 1 : 0); // nemat mesto vo width ama evo i ovde najde primena
-	length = ft_strlen((const char*)elem->data);
-	if (elem->precision > -1 && elem->precision > length - sign)
-		handle_prec(elem, length, sign);
-	if (ft_strchr(elem->flags, '+') && is_signed(elem->argtype)	&& !sign)
-	{
-		temp = ft_strjoin("+", (const char *)elem->data);
-		sign = 1;
-	}
-	else if (ft_strchr(elem->flags, ' ') && is_signed(elem->argtype) && !sign)
-	{
-		temp = ft_strjoin(" ", (const char*)elem->data);
-		sign = 1;
-	}
-	if (temp)
-	{
-		ft_memdel(&(elem->data));
-		elem->data = (void*)temp;
-	}
-	add_padding(elem, sign, add_hash(elem));
-}
+	// char		*c;
+	// temp = ft_strnew(i -1);
+	// c = ft_strnew(1);
+	// while (--i)
+	// {
+	// 	*number = *number * 10;
+	// 	*c = ((int)(*number) + '0');
+	// 	ft_strcat(temp, c);
+	// }
 
 /*
 ** ->ovde proverka za == 0 i '#' flag, za da se trgnit, zosto add_hash se povikvit
@@ -179,40 +142,3 @@ void	number_handler(t_elem *elem)
 		while (((char*)elem->data)[++i])
 			((char*)elem->data)[i] = ft_toupper(((char*)elem->data)[i]);
 }
-
-int		add_hash(t_elem *elem)
-{
-	int		hash;
-	char	*temp;
-	char	*result;
-
-	hash = 0;
-	temp = NULL;
-	if (ft_strchr(elem->flags, '#') && getbase(elem->argtype) != 10)
-		hash = 1;
-	//	hash = (((char *)elem->data)[0] == '0' ? 0 : 1);
-	if (hash)
-	{
-		if (getbase(elem->argtype) == 16) // || == 'p' za pointer
-			hash++;
-		temp = ft_strnew(hash);
-		ft_strncpy(temp, "0x", hash);
-		result = ft_strjoin(temp, elem->data);
-		temp = elem->data;
-		elem->data = result;
-		ft_memdel((void **)&temp);
-		return (hash); //mozam da go trgnam i samo return hash nadvor od ova
-	}
-	return (0);
-}
-
-/*
-converts a signed integer into decimal representation [-]dddd.
-Precision specifies the minimum number of digits to appear. 
-The default precision is 1.
-If both the converted value and the precision are ​0​ the conversion 
-results in no characters.
-
-
-error: flag '#' results in undefined behavior with 'd' conversion specifier
-*/
